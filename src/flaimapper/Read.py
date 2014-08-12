16426461dc28080ab454104800257f6dd1bc0707
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2014-08-12 12:08:12 +0200

    Create Read.py

---
+++ src/flaimapper/Read.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""FlaiMapper: computational annotation of small ncRNA derived fragments using RNA-seq high throughput data
+
+ Here we present Fragment Location Annotation Identification mapper
+ (FlaiMapper), a method that extracts and annotates the locations of
+ sncRNA-derived RNAs (sncdRNAs). These sncdRNAs are often detected in
+ sequencing data and observed as fragments of their  precursor sncRNA.
+ Using small RNA-seq read alignments, FlaiMapper is able to annotate
+ fragments primarily by peak-detection on the start and  end position
+ densities followed by filtering and a reconstruction processes.
+ Copyright (C) 2011-2014:
+ - Youri Hoogstrate
+ - Elena S. Martens-Uzunova
+ - Guido Jenster
+ 
+ 
+ [License: GPL3]
+ 
+ This file is part of flaimapper.
+ 
+ flaimapper is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ flaimapper is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+ Documentation as defined by:
+ <http://epydoc.sourceforge.net/manual-fields.html#fields-synonyms>
+"""
+
+# @TODO figure out how this can be integrated with or replaced by the "AlignedRead"-class from the pysam library
+
+class Read:
+	def __init__(self,start,stop,name=None,sequence=None):
+		self.start = start
+		self.stop = stop
+		self.name = name
+		self.sequence = sequence
+	
+	def size(self):
+		return (self.stop - self.start) - 1
