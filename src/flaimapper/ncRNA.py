29e9840bff2f41101bf5f857c72069de6ac67037
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2014-08-12 12:06:17 +0200

    Create ncRNA.py

---
+++ src/flaimapper/ncRNA.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+"""FlaiMapper: computational annotation of small ncRNA derived fragments using RNA-seq high throughput data
+
+ Here we present Fragment Location Annotation Identification mapper
+ (FlaiMapper), a method that extracts and annotates the locations of
+ sncRNA-derived RNAs (sncdRNAs). These sncdRNAs are often detected in
+ sequencing data and observed as fragments of their  precursor sncRNA.
+ Using small RNA-seq read alignments, FlaiMapper is able to annotate
+ fragments primarily by peak-detection on the start and  end position
+ densities followed by filtering and a reconstruction processes.
+ Copyright (C) 2011-2014:
+ - Youri Hoogstrate
+ - Elena S. Martens-Uzunova
+ - Guido Jenster
+ 
+ 
+ [License: GPL3]
+ 
+ This file is part of flaimapper.
+ 
+ flaimapper is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ flaimapper is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+ Documentation as defined by:
+ <http://epydoc.sourceforge.net/manual-fields.html#fields-synonyms>
+"""
+
+class ncRNA:
+	def __init__(self,name):
+		self.params = {'name':name,'sequence':False,'fasta_header':'unknown','aliases':[]}
+		self.fragments = []
+	
+	def set_parameter(self,param,value):
+		self.params[param] = value
+	
+	def get_parameter(self,param):
+		if(self.params.has_key(param)):
+			return self.params[param]
+		return None
+	
+	def get_name(self):
+		return self.params['name']
+	
+	def add_fragments(self,fragment):
+		self.fragments.append(fragment)
+	
+	def get_fragments(self):
+		return self.fragments
